64f36bf33f019b1e0618447579caca8817b44ffb
esmanurbicer <115987635+esmanurbicer@users.noreply.github.com>
2022-11-21 10:15:08 +0300

    Add files via upload

---
+++ interfacesDemo/OutsourceWorker.java
@@ -0,0 +1,10 @@
+package interfacesDemo;
+//firma çalışanı
+public class OutsourceWorker implements IWorkable{
+
+	@Override
+	public void work() {
+		
+	}
+
+}
---
+++ interfacesDemo/Robot.java
@@ -0,0 +1,8 @@
+package interfacesDemo;
+
+public class Robot implements IWorkable{
+
+	@Override
+	public void work() {}
+		
+}
